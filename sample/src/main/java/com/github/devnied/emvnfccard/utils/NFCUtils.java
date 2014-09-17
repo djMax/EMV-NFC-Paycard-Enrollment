f0e8c21140eaf317320faf74a3451b14bd484120
Max Metral <mmetral@paypal.com>
2014-09-17 10:15:55 -0400

    update for android studio and kitkat

--- sample/src/main/java/com/github/devnied/emvnfccard/utils/NFCUtils.java
+++ sample/src/main/java/com/github/devnied/emvnfccard/utils/NFCUtils.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
+import android.nfc.tech.NfcA;
 
 /**
  * 
@@ -76,6 +78,7 @@ public class NFCUtils {
 	public void disableDispatch() {
 		if (mNfcAdapter != null) {
 			mNfcAdapter.disableForegroundDispatch(mActivity);
+            mNfcAdapter.disableReaderMode(mActivity);
 		}
 	}
 
@@ -85,6 +88,35 @@ public class NFCUtils {
 	public void enableDispatch() {
 		if (mNfcAdapter != null) {
 			mNfcAdapter.enableForegroundDispatch(mActivity, mPendingIntent, INTENT_FILTER, TECH_LIST);
+            mNfcAdapter.enableReaderMode(mActivity, new NfcAdapter.ReaderCallback() {
+                @Override
+                public void onTagDiscovered(Tag tag) {
+                    IsoDep iso = IsoDep.get(tag);
+                    try {
+                        iso.connect();
+                        bytes[] hist = iso.getHistoricalBytes();
+                        //byte[] tx = {(byte) 0x00, (byte) 0xA4, (byte) 0x04, (byte) 0x00, (byte) 0xA0, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x03, (byte) 0x10, (byte) 0x10};
+                        byte[] SELECT = {
+                                (byte) 0x00, // CLA = 00 (first interindustry command set)
+                                (byte) 0xA4, // INS = A4 (SELECT)
+                                (byte) 0x04, // P1  = 04 (select file by DF name)
+                                (byte) 0x0C, // P2  = 0C (first or only file; no FCI)
+                                (byte) 0x06, // Lc  = 6  (data/AID has 6 bytes)
+                                (byte) 0x31, (byte) 0x35,(byte) 0x38,(byte) 0x34,(byte) 0x35,(byte) 0x46 // AID = 15845F
+                        };
+                        byte[] NATIVE_SELECT_APP_COMMAND = new byte[]
+                                {
+                                        (byte) 0x90, (byte) 0x5A, (byte) 0x00, (byte) 0x00, 3,  // SELECT
+                                        (byte) 0x5F, (byte) 0x84, (byte) 0x15, (byte) 0x00      // APPLICATION ID
+                                };
+                        byte[] response = iso.transceive(SELECT);
+                        System.out.println(response.toString());
+                    } catch (Exception x) {
+                        System.out.println(x.toString());
+                    }
+                }
+            },  NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK | NfcAdapter.FLAG_READER_NFC_A,
+                    null);
 		}
 	}
 
